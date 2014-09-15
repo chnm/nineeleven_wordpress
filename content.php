862fcfdf84bf2f979d648cee7e2da29389176b20
Jeri Wieringa <jeri.elizabeth@gmail.com>
2014-09-15 14:33:22 -0400

    setting up _s based theme

---
+++ content.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package NineElevenDigitalArchive
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php nineeleven_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'nineeleven' ), 
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+		?>
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'nineeleven' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php nineeleven_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
